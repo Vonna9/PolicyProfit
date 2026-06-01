cfcf503824b88b6b32ea852779d2b9e114b7e568
Vonna9 <197882100+Vonna9@users.noreply.github.com>
2026-06-01 07:28:38 -0700

    added cover page for game, configured server, setup code for the game to index.html and java file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './style.css'
+import Phaser from 'phaser' 
+const config = {
+  type: Phaser.WEBGL, 
+  width: 800,
+  height: 600, 
+  canvas: gameCanvas
+};
+
+const game = new Phaser.Game(config);
