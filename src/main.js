7b7afd2d23818aa3523796e95575aa5926f7fb9d
Vonna9 <197882100+Vonna9@users.noreply.github.com>
2026-06-05 17:24:50 -0700

    added music and began setting the scenes for the game

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import './style.css'
 import Phaser from 'phaser' 
+
+ class MyScene extends Phaser.Scene {
+  
+ }
+
 const config = {
   type: Phaser.WEBGL, 
   width: 800,
   height: 600, 
   canvas: gameCanvas
+  
 };
 
 const game = new Phaser.Game(config);
+
